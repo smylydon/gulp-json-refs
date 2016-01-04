ff84779490508ef6c4a8f18cf2f0bf75d00ffd65
Scott Morris <smorris@lb.com>
2016-01-04 13:59:18 -0500

    Added YAML support, bumped to v0.1.1

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var es = require("event-stream");
 var clone = require("clone");
 var JsonRefs = require("json-refs");
+var YAML = require("yaml-js");
 
 function getJsonMatch (filePath) {
   if (typeof filePath === "string") {
-    return filePath.match(/.json$/);
+    return filePath.match(/\.json$/);
+  }
+}
+
+function getYamlMatch (filePath) {
+  if (typeof filePath === "string") {
+    return filePath.match(/\.yaml$/);
   }
 }
 
@@ -13,9 +20,19 @@ module.exports = function(opts) {
   opts = opts || {};
 
   function resolveRefs(file, callback) {
-    var options = clone(opts);
+    var options = clone(opts),
+        fileContents;
 
-    if (file.isNull() || !getJsonMatch(file.path)) {
+    if (file.isNull()) {
+      return callback(null, file);
+    }
+
+    // Parse YAML or JSON, depending on settings
+    if (options.yaml && getYamlMatch(file.path)) {
+      fileContents = YAML.load(file.contents);
+    } else if (getJsonMatch(file.path)) {
+      fileContents = JSON.parse(file.contents);
+    } else {
       return callback(null, file);
     }
 
@@ -24,8 +41,9 @@ module.exports = function(opts) {
       options.location = file.path.split("/").slice(0,-1).join("/");
     }
 
-    JsonRefs.resolveRefs(JSON.parse(file.contents), options).then(function (results) {
-      file.contents = new Buffer(JSON.stringify(results.resolved));
+    JsonRefs.resolveRefs(fileContents, options).then(function (results) {
+      var output = (options.yaml) ? YAML.dump(results.resolved) : JSON.stringify(results.resolved);
+      file.contents = new Buffer(output);
       callback(null, file);
     }, function (error) {
       callback();
--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var gulpJsonRefs = require("../index.js");
 var es = require("event-stream");
 var path = require("path");
 var assert = require("assert");
+var YAML = require("yaml-js");
 
 require("mocha");
 
@@ -20,62 +21,120 @@ describe("gulp-json-refs", function() {
     });
   }
 
-  it ("should resolve same file refs", function (done) {
-    gulp.src(path.join(__dirname, "./fixtures/same-file-reference.json"))
-      .pipe(gulpJsonRefs())
-      .pipe(compareResults(done, {
-        expected: JSON.stringify({
-          "name": {
-            "first": "Bob",
-            "last": "Loblaw"
-          },
-          "owner": {
-            "first": "Bob",
-            "last": "Loblaw"
-          }
-        })
-      }));
+  describe("json parsing", function () {
+    it ("should resolve same file refs", function (done) {
+      gulp.src(path.join(__dirname, "./fixtures/same-file-reference.json"))
+        .pipe(gulpJsonRefs())
+        .pipe(compareResults(done, {
+          expected: JSON.stringify({
+            "name": {
+              "first": "Bob",
+              "last": "Loblaw"
+            },
+            "owner": {
+              "first": "Bob",
+              "last": "Loblaw"
+            }
+          })
+        }));
+    });
+
+    it ("should resolve local file refs", function (done) {
+      gulp.src(path.join(__dirname, "./fixtures/local-file-reference.json"))
+        .pipe(gulpJsonRefs())
+        .pipe(compareResults(done, {
+          expected: JSON.stringify({
+            "author": {
+              "first": "Bob",
+              "last": "Loblaw"
+            }
+          })
+        }));
+    });
+
+    it ("should resolve references in a referenced file", function (done) {
+      gulp.src(path.join(__dirname, "./fixtures/refer-to-reference.json"))
+        .pipe(gulpJsonRefs())
+        .pipe(compareResults(done, {
+          expected: JSON.stringify({
+            "title": "json",
+            "author": {
+              "first": "Bob",
+              "last": "Loblaw"
+            }
+          })
+        }));
+    });
+
+    it ("should resolve remote refs", function (done) {
+      gulp.src(path.join(__dirname, "./fixtures/remote-reference.json"))
+        .pipe(gulpJsonRefs())
+        .pipe(compareResults(done, {
+          expected: JSON.stringify({
+            "name": "json-refs",
+            "owner": "whitlockjc"
+          })
+        }));
+    });
   });
 
-  it ("should resolve local file refs", function (done) {
-    gulp.src(path.join(__dirname, "./fixtures/local-file-reference.json"))
-      .pipe(gulpJsonRefs())
-      .pipe(compareResults(done, {
-        expected: JSON.stringify({
-          "author": {
-            "first": "Bob",
-            "last": "Loblaw"
-          }
-        })
-      }));
-  });
+  describe("yaml parsing", function () {
+    it ("should resolve same file refs", function (done) {
+      gulp.src(path.join(__dirname, "./fixtures/same-file-reference.yaml"))
+        .pipe(gulpJsonRefs({ "yaml": true }))
+        .pipe(compareResults(done, {
+          expected: YAML.dump({
+            "name": {
+              "first": "Bob",
+              "last": "Loblaw"
+            },
+            "owner": {
+              "first": "Bob",
+              "last": "Loblaw"
+            }
+          })
+        }));
+    });
 
-  it ("should resolve references in a referenced file", function (done) {
-    gulp.src(path.join(__dirname, "./fixtures/refer-to-reference.json"))
-      .pipe(gulpJsonRefs())
-      .pipe(compareResults(done, {
-        expected: JSON.stringify({
-          "title": "json",
-          "author": {
-            "first": "Bob",
-            "last": "Loblaw"
-          }
-        })
-      }));
-  });
+    it ("should resolve local JSON file refs", function (done) {
+      gulp.src(path.join(__dirname, "./fixtures/local-file-reference.yaml"))
+        .pipe(gulpJsonRefs({ "yaml": true }))
+        .pipe(compareResults(done, {
+          expected: YAML.dump({
+            "author": {
+              "first": "Bob",
+              "last": "Loblaw"
+            }
+          })
+        }));
+    });
 
-  it ("should resolve remote refs", function (done) {
-    gulp.src(path.join(__dirname, "./fixtures/remote-reference.json"))
-      .pipe(gulpJsonRefs())
-      .pipe(compareResults(done, {
-        expected: JSON.stringify({
-          "name": "json-refs",
-          "owner": "whitlockjc"
-        })
-      }));
-  });
+    it ("should resolve references in a referenced JSON file", function (done) {
+      gulp.src(path.join(__dirname, "./fixtures/refer-to-reference.yaml"))
+        .pipe(gulpJsonRefs({ "yaml": true }))
+        .pipe(compareResults(done, {
+          expected: YAML.dump({
+            "title": "json",
+            "author": {
+              "first": "Bob",
+              "last": "Loblaw"
+            }
+          })
+        }));
+    });
 
-  it ("should accept a non-JSON file but do nothing to it", function (done) {
+    it ("should resolve remote JSON refs", function (done) {
+      gulp.src(path.join(__dirname, "./fixtures/remote-reference.yaml"))
+        .pipe(gulpJsonRefs({ "yaml": true }))
+        .pipe(compareResults(done, {
+          expected: YAML.dump({
+            "name": "json-refs",
+            "owner": "whitlockjc"
+          })
+        }));
+    });  });
+
+  it ("should accept a non-JSON / non-YAML file but do nothing to it", function (done) {
     gulp.src(path.join(__dirname, "./fixtures/invalid-json.html"))
       .pipe(gulpJsonRefs())
       .pipe(compareResults(done, {
