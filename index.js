7b4444bd8a465cf2344e359d28c24caff4831b1b
Michael Godden <smylydon@yahoo.com>
2016-10-03 14:03:44 -0700

    Fixed it so we can use gulp-json-refs with streams.

--- index.js
+++ index.js
@@ -3,31 +3,66 @@ var clone = require("clone");
 var JsonRefs = require("json-refs");
 var YAML = require("js-yaml");
 
-module.exports = function(opts) {
-  "use strict";
-  opts = opts || {};
+module.exports.resolveRefsAt = {
+        resolveRefsAt: function (opts) {
+            "use strict";
+            opts = opts || {};
 
-  function resolveRefs(file, callback) {
-    var options = clone(opts);
+            function resolveRefs(file, callback) {
+                var options = clone(opts);
 
-    if (file.isNull()) {
-      return callback(null, file);
-    }
+                if (file.isNull()) {
+                    return callback(null, file);
+                }
 
-    options.loaderOptions = {
-        processContent: function (res, callback) {
-          callback(undefined, YAML.safeLoad(res.text));
+                options.loaderOptions = {
+                    processContent: function (res, callback) {
+                        callback(undefined, YAML.safeLoad(res.text));
+                    }
+                };
+
+                JsonRefs.resolveRefsAt(file.path, options).then(function (results) {
+                        var output = (options.yaml) ? YAML.safeDump(results.resolved, {
+                            noRefs: true
+                        }) : JSON.stringify(results.resolved);
+                        file.contents = new Buffer(output);
+                        callback(null, file);
+                    }).catch(function (error) {
+                        callback();
+                    });
+                }
+
+                return es.map(resolveRefs);
+            },
+
+            resolveRefs: function (opts) {
+                "use strict";
+                opts = opts || {};
+
+                function resolveRefs(file, callback) {
+                    var options = clone(opts);
+
+                    if (file.isNull()) {
+                        return callback(null, file);
+                    }
+
+                    options.loaderOptions = {
+                        processContent: function (res, callback) {
+                            callback(undefined, YAML.safeLoad(res.text));
+                        }
+                    };
+
+                    var jsonFile = JSON.parse(file.contents.toString('utf8'));
+                    JsonRefs.resolveRefs(jsonFile, options).then(function (results) {
+                        var output = JSON.stringify(results.resolved);
+                        file.contents = new Buffer(output);
+                        callback(null, file);
+                    }).catch(function (error) {
+                        callback();
+                    });
+                }
+
+                return es.map(resolveRefs);
+            }
+
         }
-    };
-
-    JsonRefs.resolveRefsAt(file.path, options).then(function (results) {
-      var output = (options.yaml) ? YAML.safeDump(results.resolved, {noRefs: true}) : JSON.stringify(results.resolved);
-      file.contents = new Buffer(output);
-      callback(null, file);
-    }, function (error) {
-      callback();
-    });
-  }
-
-  return es.map(resolveRefs);
-};
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var gulp = require("gulp");
-var gulpJsonRefs = require("../index.js");
+var gulpJsonRefs = require("../index.js").resolveRefsAt;
 var es = require("event-stream");
 var path = require("path");
 var assert = require("assert");
@@ -7,132 +7,141 @@ var YAML = require("js-yaml");
 
 require("mocha");
 
-describe("gulp-json-refs", function() {
-  "use strict";
+describe("gulp-json-refs", function () {
+    "use strict";
 
-  function compareResults (done, options) {
-    options = options || {};
+    function compareResults(done, options) {
+        options = options || {};
 
-    return es.map(function (file) {
-      var result = String(file.contents);
-      var expected = options.expected;
-      assert.equal(result, expected);
-      done();
-    });
-  }
+        return es.map(function (file) {
+            var result = String(file.contents);
+            var expected = options.expected;
+            assert.equal(result, expected);
+            done();
+        });
+    }
 
-  describe("json parsing", function () {
-    it ("should resolve same file refs", function (done) {
-      gulp.src(path.join(__dirname, "./fixtures/same-file-reference.json"))
-        .pipe(gulpJsonRefs())
-        .pipe(compareResults(done, {
-          expected: JSON.stringify({
-            "name": {
-              "first": "Bob",
-              "last": "Loblaw"
-            },
-            "owner": {
-              "first": "Bob",
-              "last": "Loblaw"
-            }
-          })
-        }));
-    });
+    describe("json parsing", function () {
+        it("should resolve same file refs", function (done) {
+            gulp.src(path.join(__dirname, "./fixtures/same-file-reference.json"))
+                .pipe(gulpJsonRefs())
+                .pipe(compareResults(done, {
+                    expected: JSON.stringify({
+                        "name": {
+                            "first": "Bob",
+                            "last": "Loblaw"
+                        },
+                        "owner": {
+                            "first": "Bob",
+                            "last": "Loblaw"
+                        }
+                    })
+                }));
+        });
 
-    it ("should resolve local file refs", function (done) {
-      gulp.src(path.join(__dirname, "./fixtures/local-file-reference.json"))
-        .pipe(gulpJsonRefs())
-        .pipe(compareResults(done, {
-          expected: JSON.stringify({
-            "author": {
-              "first": "Bob",
-              "last": "Loblaw"
-            }
-          })
-        }));
-    });
+        it("should resolve local file refs", function (done) {
+            gulp.src(path.join(__dirname, "./fixtures/local-file-reference.json"))
+                .pipe(gulpJsonRefs())
+                .pipe(compareResults(done, {
+                    expected: JSON.stringify({
+                        "author": {
+                            "first": "Bob",
+                            "last": "Loblaw"
+                        }
+                    })
+                }));
+        });
 
-    it ("should resolve references in a referenced file", function (done) {
-      gulp.src(path.join(__dirname, "./fixtures/refer-to-reference.json"))
-        .pipe(gulpJsonRefs())
-        .pipe(compareResults(done, {
-          expected: JSON.stringify({
-            "title": "json",
-            "author": {
-              "first": "Bob",
-              "last": "Loblaw"
-            }
-          })
-        }));
-    });
+        it("should resolve references in a referenced file", function (done) {
+            gulp.src(path.join(__dirname, "./fixtures/refer-to-reference.json"))
+                .pipe(gulpJsonRefs())
+                .pipe(compareResults(done, {
+                    expected: JSON.stringify({
+                        "title": "json",
+                        "author": {
+                            "first": "Bob",
+                            "last": "Loblaw"
+                        }
+                    })
+                }));
+        });
 
-    it ("should resolve remote refs", function (done) {
-      gulp.src(path.join(__dirname, "./fixtures/remote-reference.json"))
-        .pipe(gulpJsonRefs())
-        .pipe(compareResults(done, {
-          expected: JSON.stringify({
-            "name": "json-refs",
-            "owner": "whitlockjc"
-          })
-        }));
-    });
-  });
-
-  describe("yaml parsing", function () {
-    it ("should resolve same file refs", function (done) {
-      gulp.src(path.join(__dirname, "./fixtures/same-file-reference.yaml"))
-        .pipe(gulpJsonRefs({ "yaml": true }))
-        .pipe(compareResults(done, {
-          expected: YAML.safeDump({
-            "name": {
-              "first": "Bob",
-              "last": "Loblaw"
-            },
-            "owner": {
-              "first": "Bob",
-              "last": "Loblaw"
-            }
-          })
-        }));
+        it("should resolve remote refs", function (done) {
+            gulp.src(path.join(__dirname, "./fixtures/remote-reference.json"))
+                .pipe(gulpJsonRefs())
+                .pipe(compareResults(done, {
+                    expected: JSON.stringify({
+                        "name": "json-refs",
+                        "owner": "whitlockjc"
+                    })
+                }));
+        });
     });
 
-    it ("should resolve local JSON file refs", function (done) {
-      gulp.src(path.join(__dirname, "./fixtures/local-file-reference.yaml"))
-        .pipe(gulpJsonRefs({ "yaml": true }))
-        .pipe(compareResults(done, {
-          expected: YAML.safeDump({
-            "author": {
-              "first": "Bob",
-              "last": "Loblaw"
-            }
-          })
-        }));
-    });
+    describe("yaml parsing", function () {
+        it("should resolve same file refs", function (done) {
+            gulp.src(path.join(__dirname, "./fixtures/same-file-reference.yaml"))
+                .pipe(gulpJsonRefs({
+                    "yaml": true
+                }))
+                .pipe(compareResults(done, {
+                    expected: YAML.safeDump({
+                        "name": {
+                            "first": "Bob",
+                            "last": "Loblaw"
+                        },
+                        "owner": {
+                            "first": "Bob",
+                            "last": "Loblaw"
+                        }
+                    })
+                }));
+        });
 
-    it ("should resolve references in a referenced JSON file", function (done) {
-      gulp.src(path.join(__dirname, "./fixtures/refer-to-reference.yaml"))
-        .pipe(gulpJsonRefs({ "yaml": true }))
-        .pipe(compareResults(done, {
-          expected: YAML.safeDump({
-            "title": "json",
-            "author": {
-              "first": "Bob",
-              "last": "Loblaw"
-            }
-          })
-        }));
-    });
+        it("should resolve local JSON file refs", function (done) {
+            gulp.src(path.join(__dirname, "./fixtures/local-file-reference.yaml"))
+                .pipe(gulpJsonRefs({
+                    "yaml": true
+                }))
+                .pipe(compareResults(done, {
+                    expected: YAML.safeDump({
+                        "author": {
+                            "first": "Bob",
+                            "last": "Loblaw"
+                        }
+                    })
+                }));
+        });
 
-    it ("should resolve remote JSON refs", function (done) {
-      gulp.src(path.join(__dirname, "./fixtures/remote-reference.yaml"))
-        .pipe(gulpJsonRefs({ "yaml": true }))
-        .pipe(compareResults(done, {
-          expected: YAML.safeDump({
-            "name": "json-refs",
-            "owner": "whitlockjc"
-          })
-        }));
-    });  });
+        it("should resolve references in a referenced JSON file", function (done) {
+            gulp.src(path.join(__dirname, "./fixtures/refer-to-reference.yaml"))
+                .pipe(gulpJsonRefs({
+                    "yaml": true
+                }))
+                .pipe(compareResults(done, {
+                    expected: YAML.safeDump({
+                        "title": "json",
+                        "author": {
+                            "first": "Bob",
+                            "last": "Loblaw"
+                        }
+                    })
+                }));
+        });
+
+        it("should resolve remote JSON refs", function (done) {
+            gulp.src(path.join(__dirname, "./fixtures/remote-reference.yaml"))
+                .pipe(gulpJsonRefs({
+                    "yaml": true
+                }))
+                .pipe(compareResults(done, {
+                    expected: YAML.safeDump({
+                        "name": "json-refs",
+                        "owner": "whitlockjc"
+                    })
+                }));
+        });
+    });
 });
 
 // Use cases
